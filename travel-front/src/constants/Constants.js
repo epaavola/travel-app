6d692d094eda4759f47c083a6a46b1aee107bd9c
esapa <paavola.esa@gmail.com>
2021-02-08 21:32:30 +0200

    new jar package

--- travel-front/src/constants/Constants.js
+++ travel-front/src/constants/Constants.js
@@ -1,7 +1,7 @@
 const prod = {
     url: {
       SITE_URL: 'https://travel.paavolagroup.fi',
-      KEYCLOAK_BASE_URL: 'https://hallinta.travel.paavolagroup.fi/auth',
+      KEYCLOAK_BASE_URL: 'https://hallinta.missio.paavolagroup.fi/auth',
       API_BASE_URL: 'https://api.travel.paavolagroup.fi',
       MISSIONS_API_URL: 'https://api.travel.paavolagroup.fi/api/v1/missions/',
       FILE_API_URL: 'https://api.travel.paavolagroup.fi/api/v1/files/'
@@ -16,9 +16,9 @@ const prod = {
     url: {
       SITE_URL: 'http://localhost:3000',
       KEYCLOAK_BASE_URL: 'http://localhost:8080/auth',
-      API_BASE_URL: 'http://localhost:8000',
-      MISSIONS_API_URL: 'http://localhost:8000/api/v1/missions/',
-      FILE_API_URL: 'http://localhost:8000/api/v1/files/'
+      API_BASE_URL: 'http://localhost:7000',
+      MISSIONS_API_URL: 'http://localhost:7000/api/v1/missions/',
+      FILE_API_URL: 'http://localhost:7000/api/v1/files/'
     },
     keycloak: {
       REALM: "Travel-app",
